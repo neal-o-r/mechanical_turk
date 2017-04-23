c1f89c0b4bf6a4f0d9646da4a023f8f8bcbe9534
neal-o-r <neal@n-o-r.xyz>
2017-04-23 12:38:56 +0100

    alpha beta

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
 # Mechanical Turk
 ---
 
-A chess playing bot. Currently can play in a few ways: 
+A chess playing bot. Currently can play in a few ways, in increasing order of ability:
+ 
 	* Randomly 
 	* Evaluate one move ahead
 	* Evaluate n moves ahead with interatively deepened Minimax.
+	* Evaluate n moves ahead with interatively deepened $\alpha - \beta$ pruned Minimax.
--- players.py
+++ players.py
@@ -59,6 +59,40 @@ def mm_player(board):
 	return move 
 
 
+def ab_player(board):
+	# minimax player
+
+	best_score = 99999
+	move = None
+	n_moves = len(board.legal_moves)
+	i = 0
+	for m in board.generate_legal_moves():
+		new_board = board.copy()
+		new_board.push(m)
+		score = ab_iterative_deepen(new_board, n_moves)
+		#score = minimax(new_board, 1, True)
+		if score < best_score:
+			best_score = score
+			move = m
+
+	return move
+
+
+def ab_iterative_deepen(board, n):
+
+	score = alpha_beta(board, 1, -99999, 99999, True)
+
+	d = 5
+	with timeout(seconds=1/n):
+		for i in range(2, d):
+			try:
+				s = alpha_beta(board, i, -99999, 99999, True)
+			except:
+				break
+			print(i)
+			score = s
+	return score
+
 def mm_iterative_deepen(board, n):
 
 	score = minimax(board, 1, True)
@@ -99,4 +133,41 @@ def minimax(board_in, depth, turn):
 			best_value = min(best_value, v)
 
 		return best_value
+
+
+def alpha_beta(board_in, depth, alpha, beta, turn):
+
+	if (depth == 0) or (board_in.is_game_over(claim_draw=True)):
+		return evaluation.piece_evaluate(board_in)		
+
+	if turn:
+		v = -99999
+		for m in board_in.generate_legal_moves():
+			board = board_in.copy()
+			board.push(m)
+
+			v = max(v, alpha_beta(board, depth-1, alpha, beta, False))
+			alpha = max(alpha, v)
+			if beta <= alpha: 
+				break
+
+		return v
+
+	else:
+		v = 99999
+		for m in board_in.generate_legal_moves():
+			board = board_in.copy()
+			board.push(m)
+
+			v = min(v, alpha_beta(board, depth-1, alpha, beta, True))
+			beta = min(beta, v)
+			if beta <= alpha: 
+				break
+
+		return v
+
+
+
+
+
 
